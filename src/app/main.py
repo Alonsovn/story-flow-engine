06f518a798523332cae44c1871dfa20d939be7fc
Alonso VN <alonsonh94@gmail.com>
2026-03-26 21:19:41 -0600

    hello world

--- src/app/main.py
+++ src/app/main.py
@@ -1,11 +1,9 @@
-import typer
 
-app = typer.Typer()
 
-@app.command()
-def greet():
-    """Print Hello World!"""
+def hello():
     print("Hello World!")
 
 if __name__ == "__main__":
-    app()
+    hello()
+
+
